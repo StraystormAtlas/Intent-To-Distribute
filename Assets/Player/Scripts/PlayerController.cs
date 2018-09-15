8e3f79a5739a67f318debf22d6d0356c817ab05b
StraystormAtlas <StraystormAtlas@gmail.com>
2018-09-14 19:23:17 -0600

    Animation System
    (Very Broken)

---
+++ Assets/Player/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+public class PlayerController : MonoBehaviour
+{
+	public Animator animator;
+	void Start()
+	{
+		
+	}
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 5.0f;
+		animator.SetBool("Walking",Input.GetButton("Vertical")||Input.GetButton("Horizontal"));
+		animator.SetBool("Running",Input.GetButton("RunMod"));
+        transform.Translate(x, 0, z);
+		if(Input.GetButton("Jump"))
+		{
+		transform.Translate(0, 0.8f, 0);
+		}
+    }
+}
